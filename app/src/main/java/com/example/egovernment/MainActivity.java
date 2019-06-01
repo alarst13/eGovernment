f08e8a4fe195df17367b5e29e13e49d51a875ac6
Mehrkish <MehrkishA8@gmail.com>
2019-06-01 16:58:31 -0700

    Library is now created

--- app/src/main/java/com/example/egovernment/MainActivity.java
+++ app/src/main/java/com/example/egovernment/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffb2b2")));
 
+
         if (Build.VERSION.SDK_INT >= 21) {
             Window window = this.getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
